893f90736f263e44bc4dddb7b901b0597c176dd0
Phil Burrows <github@philburrows.com>
2019-11-19 17:32:46 -0500

    computer please computer

--- auto-assign.js
+++ auto-assign.js
@@ -5,7 +5,9 @@ let autoAssign = function(milliseconds) {
   return new Promise((resolve, reject) => {
     // I want to just see things in here
     console.log("my github context");
-    console.log(JSON.stringify(github.context.payload.draft));
+    console.log(JSON.stringify(github.context.payload.pull_request));
+    console.log("draft?");
+    console.log(JSON.stringify(github.context.payload.pull_request.draft));
 
     setTimeout(() => resolve("done!"), 1000);
   });
