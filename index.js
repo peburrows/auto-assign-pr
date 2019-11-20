ba3c2d2f10f41e0e40cc29bec2d7039ebe9260a5
Phil Burrows <github@philburrows.com>
2019-11-20 11:39:32 -0500

    now actually auto-assign stuff...?

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const autoAssign = require("./auto-assign");
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput("milliseconds");
-    console.log(`Waiting ${ms} milliseconds ...`);
-
-    core.debug(new Date().toTimeString());
-    // wait(parseInt(ms));
     autoAssign();
-    core.debug(new Date().toTimeString());
+    // const ms = core.getInput("milliseconds");
+    // console.log(`Waiting ${ms} milliseconds ...`);
 
-    core.setOutput("time", new Date().toTimeString());
+    // core.debug(new Date().toTimeString());
+    // // wait(parseInt(ms));
+    // autoAssign();
+    // core.debug(new Date().toTimeString());
+
+    // core.setOutput("time", new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
