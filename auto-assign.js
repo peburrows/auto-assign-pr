739ef1aff0a3fec080a104862ebfea7c68dd3b3c
Phil Burrows <github@philburrows.com>
2020-02-07 10:25:51 -0600

    add support for requesting multiple reviewers

--- auto-assign.js
+++ auto-assign.js
@@ -1,12 +1,37 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+const shuffleArray = function(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
+
 let autoAssign = function() {
   return new Promise(async (resolve, reject) => {
     // we should remove the current author from this list
     let reviewerList = JSON.parse(core.getInput("draft-approvers"), {
       required: true
     });
+
+    let reviewerCount = parseInt(core.getInput("reviewer-count"));
+    if (isNaN(reviewerCount)) {
+      reviewerCount = 1;
+    }
 
     const {
       number: pull_number,
@@ -24,14 +49,28 @@ let autoAssign = function() {
     const token = core.getInput("github-token", { required: true });
     const octokit = new github.GitHub(token);
 
-    if (draft && requested_reviewers.length === 0) {
-      console.log("draft PR with no current reviewers, so will add one");
+    if (draft && requested_reviewers.length < reviewerCount) {
+      console.log(
+        "draft PR with less than desired reviewer count, so we will add some"
+      );
+
+      let newReviewerCount = reviewerCount - requested_reviewers.length;
 
       // remove the author from the reviewerList
       reviewerList = reviewerList.filter(r => r !== pr_author);
-      const i = Math.floor(Math.random() * reviewerList.length);
-      const reviewers = [reviewerList[i]];
+      let reviewers = [],
+        count = 0;
 
+      // grab some random reviewer(s)
+      while (count < newReviewerCount) {
+        reviewerList = shuffleArray(reviewerList);
+        if (toRequest.indexOf(reviewerList[0]) === -1) {
+          toRequest.push(reviewerList[0]);
+          count++;
+        }
+      }
+
+      // now request review from the reviewer(s)
       await octokit.pulls.createReviewRequest({
         owner,
         repo,
