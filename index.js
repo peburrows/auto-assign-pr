ceaaa9636c4fe1e61f954a8f211fc4d41d62dcf7
Phil Burrows <github@philburrows.com>
2020-02-07 10:42:13 -0600

    things are making less and less sense to me

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ const autoAssign = require("./auto-assign");
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    console.log("running my action...");
-    autoAssign();
+  // try {
+  console.log("running my action...");
+  await autoAssign();
+  console.log("supposedly, I ran the autoAssign stuff");
 
-    // core.debug(new Date().toTimeString());
-    // // wait(parseInt(ms));
-    // autoAssign();
-    // core.debug(new Date().toTimeString());
+  // core.debug(new Date().toTimeString());
+  // // wait(parseInt(ms));
+  // autoAssign();
+  // core.debug(new Date().toTimeString());
 
-    // core.setOutput("time", new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  // core.setOutput("time", new Date().toTimeString());
+  // } catch (error) {
+  //   core.setFailed(error.message);
+  // }
 }
 
 // hello??
-console.log("about to run some stuff");
+// console.log("about to run some stuff");
 run();
